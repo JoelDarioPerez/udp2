c34abc0026f4094897d86d7440bd5012bc8b1755
Joel Perez <perezjoeldario@gmail.com>
2023-10-09 16:21:47 -0300

    a

--- handler.mjs
+++ handler.mjs
@@ -1,5 +1,5 @@
 import { AL900 } from "./protocols.mjs";
-export const handler = () => {
+export const handler = (data) => {
   if (Buffer.byteLength(data) === 99 || Buffer.byteLength(data) === 97)
     return AL900(data);
   else return "Invalid Protocol";
