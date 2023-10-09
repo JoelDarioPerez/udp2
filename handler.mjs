774238789e65c59276f476a9e363287334e1672a
Joel Perez <perezjoeldario@gmail.com>
2023-10-09 16:09:58 -0300

    a

--- handler.mjs
+++ handler.mjs
@@ -1,4 +1,4 @@
-import AL900 from "./protocols.mjs";
+import { AL900 } from "./protocols.mjs";
 export const handler = () => {
   if (Buffer.byteLength(data) === 99 || Buffer.byteLength(data) === 97)
     return AL900(data);
